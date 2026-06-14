2bab2a2afaa407c4297eaefbdb798be1b67f320f
Udacha <user@example.com>
2026-06-15 01:22:59 +0300

    feat: яркость дыма 0.1

--- assets/fluid.js
+++ assets/fluid.js
@@ -1471,9 +1471,9 @@ function correctDeltaY (delta) {
 function generateColor () {
     // purple/violet hue range: 0.70–0.82
     let c = HSVtoRGB(0.70 + Math.random() * 0.12, 0.9, 1.0);
-    c.r *= 0.7;
-    c.g *= 0.7;
-    c.b *= 0.7;
+    c.r *= 0.1;
+    c.g *= 0.1;
+    c.b *= 0.1;
     return c;
 }
 
