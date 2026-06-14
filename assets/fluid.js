754af387583d4403b4e00597146a39e6bf78184d
Udacha <user@example.com>
2026-06-15 01:43:07 +0300

    feat: снизить SPLAT_FORCE 6000→2000 (меньше яркости от быстрых движений)

--- assets/fluid.js
+++ assets/fluid.js
@@ -40,7 +40,7 @@ let config = {
     PRESSURE_ITERATIONS: 20,
     CURL: 30,
     SPLAT_RADIUS: 0.25,
-    SPLAT_FORCE: 6000,
+    SPLAT_FORCE: 2000,
     SHADING: true,
     COLORFUL: true,
     COLOR_UPDATE_SPEED: 10,
