bb0173c13f84402497863fad945b759601cfbf44
Udacha <user@example.com>
2026-06-15 06:59:15 +0300

    fix: вернуть mix-blend-mode:screen, страница снова видна

--- assets/fluid.js
+++ assets/fluid.js
@@ -45,7 +45,7 @@ let config = {
     COLORFUL: true,
     COLOR_UPDATE_SPEED: 10,
     PAUSED: false,
-    BACK_COLOR: { r: 8, g: 8, b: 15 },
+    BACK_COLOR: { r: 0, g: 0, b: 0 },
     TRANSPARENT: false,
     BLOOM: true,
     BLOOM_ITERATIONS: 8,
