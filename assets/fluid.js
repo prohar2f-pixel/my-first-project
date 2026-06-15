c355af5cfa93c934fd8c900a1d1c46264e5f5ca5
Udacha <user@example.com>
2026-06-15 07:17:05 +0300

    feat: яркость дыма 0.07

--- assets/fluid.js
+++ assets/fluid.js
@@ -1470,9 +1470,9 @@ function correctDeltaY (delta) {
 
 function generateColor () {
     let c = HSVtoRGB(Math.random(), 1.0, 1.0);
-    c.r *= 0.15;
-    c.g *= 0.15;
-    c.b *= 0.15;
+    c.r *= 0.07;
+    c.g *= 0.07;
+    c.b *= 0.07;
     return c;
 }
 
